2865bee2f20a9a4345ee6acac086bf97ec11f1d5
malawadd <50057882+malawadd@users.noreply.github.com>
2022-12-18 14:12:29 +0300

    🥞 update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,31 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    testnet: {
+      url: 'https://erpc.apothem.network',
+      accounts: [process.env.MNENOMIC],
+    },
+    xdc:{
+      url: 'https://erpc.xinfin.network',
+      accounts: [process.env.MNENOMIC],
+    }
+  },
+
+  abiExporter: [
+    {
+      path: './abi/pretty',
+      pretty: true,
+    },
+    {
+      path: './abi/ugly',
+      pretty: false,
+    },
+    {
+      path: './abi/minimal',
+      format: "minimal",
+    
+    }
+  ]
+
 };
